ab8500b557f38eecf4216d9f748f1803279f280c
SGNayak12 <GNayakSuhas12@gmail.com>
2024-06-19 18:03:15 +0530

    Done with navbar styling

---
+++ src/NavBar.jsx
@@ -0,0 +1,15 @@
+import UserCard from "./UserCard";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <>
+      <div id="Nav-bar">
+        <h3>TODO APP</h3>
+        <UserCard></UserCard>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
---
+++ src/UserCard.jsx
@@ -0,0 +1,12 @@
+import "./usercard.css";
+function UserCard() {
+  return (
+    <>
+      <div id="UserCard">
+        <img src="./profile.png" alt="" />
+        <h3>SUHAS G NAYAK</h3>
+      </div>
+    </>
+  );
+}
+export default UserCard;
