9aa3c63102baf3ed25d51c308ae148650a7b76c3
SGNayak12 <GNayakSuhas12@gmail.com>
2024-07-08 23:20:41 +0530

    initial commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const fs=require("fs")
 const app=express();
-const PORT=4000;
+const PORT=3000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
